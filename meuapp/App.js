51d572c9e1f6fe4257cad886d8a89251e1b37418
josafaverissimo <josafaverissimo98@gmail.com>
2023-10-02 23:32:56 +0000

    changes

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,32 +1,43 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-    const phrases = [
-        "Você será escolhido para algo grande!",
-        "Desconfie de quem quem está a sua volta!",
-        "Todos se levantarão contra ti",
-        "Não desista"
-    ]
-    const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0)
-    const [phrase, setPhrase] = useState(phrases[currentPhraseIndex])
+   const [name, setName] = useState('')
 
-    function changePhrase() {
-        do {
-            const randomIndex = Math.floor(Math.random() * phrases.length)
-        } while(randomIndex === currentPhraseIndex)
-
-        setCurrentPhraseIndex(randomIndex)
-        setPhrase(phrase[currentPhraseIndex])
+    function getName(input) {
+        if(input.length > 0) {
+            setName(`Bem-vindo(a) ${input}`)
+        } else {
+            setName('')
+        }
     }
     
     return (
-        <View>
-            <StatusBar></StatusBar>
+        <View style={styles.container}>
+            <TextInput 
+                style={styles.input}
+                placeholder="Type your name"
+                onChangeText={getName}
+            />
 
-            <Button onPress={changePhrase} title="Gere uma frase aleatória">
-            <Text>{phrase}</Text>
+            <Text style={{styles.texto}}>{name}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    input:  {
+        height: 45,
+        borderWidth: 1,
+        padding: 10,
+            fontSize: 20             
+    },
+    texto: {
+        textAlign: center,
+        fontSize: 25
+    }
+})
