6b9590b909c4c8165128e92923cb8198cd44815a
josafaverissimo <josafaverissimo98@gmail.com>
2023-10-03 00:29:42 +0000

    changes

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,77 +1,47 @@
 import { useState } from 'react';
-import { StatusBar, StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView} from 'react-native';
+import { StatusBar, StyleSheet, Text, View, ScrollView} from 'react-native';
 
 export default function App() {
-    const [img, setImg] = useState(require('./src/biscoito.png'))
-    const [text, setText] = useState('')
-
-    let frases = [
-        "Siga os bons e aprenda com eles",
-        "Seja honesto e ganhará o mundo",
-        "Seja torcedor do palmeiras e seja eternamente feliz",
-        "A maior barreira para o sucesso é o medo do fracasso",
-        "Deixa de lado as preocupações e seja feliz"
-    ]
-
-    function quebraBiscoito() {
-        const randomIndex = Math.floor(Math.random() * frases.length)
-
-        setImg(require('./src/biscoito-quebrado.jpg'))
-        setText(`"${frases[randomIndex]}"`)
-    }
-
-    function resetaBiscoito() {
-        setImg(require('./src/biscoito.png'))
-        setText("")
-    }
-    
     return (
-        <SafeAreaView>
-            <StatusBar />
-
-            <Image
-                source={img}
-                style={styles.img}
-            />
-
-            <Text style={styles.texto}> {text} </Text>
-            <TouchableOpacity
-                style={[styles.botao, {marginTop: 40}, {borderColor: "black"}]}
-                onPress={quebraBiscoito}
-            >
-                <View style={styles.btnarea}>
-                    <Text style={styles.btntexto}>Quebrar Biscoito</Text>
-                </View>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                style={[styles.botao, {borderColor: "black"}]}
-                onPress={resetaBiscoito}
-            >
-                <View style={styles.btnarea}>
-                    <Text style={styles.btntexto}>Resetar Biscoito</Text>
-                </View>
-            </TouchableOpacity>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <ScrollView>
+                <View style={styles.box1}></View>
+                <View style={styles.box2}></View>
+                <View style={styles.box3}></View>
+                <View style={styles.box4}></View>
+                <View style={styles.box1}></View>
+                <View style={styles.box2}></View>
+                <View style={styles.box3}></View>
+                <View style={styles.box4}></View>
+                <View style={styles.box1}></View>
+                <View style={styles.box2}></View>
+                <View style={styles.box3}></View>
+                <View style={styles.box4}></View>
+                <View style={styles.box1}></View>
+                <View style={styles.box2}></View>
+            </ScrollView>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        marginTop: 68
+        flex: 1
     },
-    img: {
+    box1: {
         height: 250,
-        width: 250
+        backgroundColor: "red"
     },
-    text: {
-        alignItems: "center"
+    box2: {
+        height: 250,
+        backgroundColor: "yellow"
     },
-    botao: {
-        justifyContent: "center",
-        width: 100,
-        height: 100,
+    box3: {
+        height: 250,
         backgroundColor: "pink"
+    },
+    box4: {
+        height: 250,
+        backgroundColor: "blue"
     }
 })
