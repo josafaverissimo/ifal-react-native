344608852e313420652feeee13633fbe06548c66
josafaverissimo <josafaverissimo98@gmail.com>
2023-10-03 00:49:54 +0000

    changes

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,25 +1,22 @@
 import { useState } from 'react';
-import { StatusBar, StyleSheet, Text, View, ScrollView} from 'react-native';
+import { StatusBar, StyleSheet, Text, View, FlatList} from 'react-native';
 
 export default function App() {
+    const [feed, setFeed] = useState([
+        {id: '1', nome: "Josafá Veríssimo", idade: '21'},
+        {id: '2', nome: "Jeremias Verissimo", idade: '19'},
+        {id: '3', nome: "Mel Issa", idade: '23'},
+        {id: '4', nome: "Icaro Ferreira", idade: '26'},
+        {id: '5', nome: "Silvio Santos", idade: '99'},
+    ])
+    
     return (
         <View style={styles.container}>
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
-                <View style={styles.box1}></View>
-                <View style={styles.box2}></View>
-                <View style={styles.box3}></View>
-                <View style={styles.box4}></View>
-                <View style={styles.box1}></View>
-                <View style={styles.box2}></View>
-                <View style={styles.box3}></View>
-                <View style={styles.box4}></View>
-                <View style={styles.box1}></View>
-                <View style={styles.box2}></View>
-                <View style={styles.box3}></View>
-                <View style={styles.box4}></View>
-                <View style={styles.box1}></View>
-                <View style={styles.box2}></View>
-            </ScrollView>
+            <FlatList
+                showsHorizontalScrollIndicator={false}
+                data={feed}
+                renderItem={({item}) => <Pessoa data={item} />}
+            />
         </View>
     );
 }
@@ -28,20 +25,23 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
-    box1: {
-        height: 250,
-        backgroundColor: "red"
+    areaPessoa: {
+        backgroundColor: 'black',
+        height: 200,
+        marginBottom: 15,
+        justifyContent: 'center'
     },
-    box2: {
-        height: 250,
-        backgroundColor: "yellow"
-    },
-    box3: {
-        height: 250,
-        backgroundColor: "pink"
-    },
-    box4: {
-        height: 250,
-        backgroundColor: "blue"
+    textoPessoa: {
+        color: '#fff',
+        fontSize: 20
     }
 })
+
+function Pessoa(props) {
+    return (
+     <View style={styles.areaPessoa}>
+         <Text style={styles.textoPessoa}>{props.data.nome}</Text>
+         <Text style={styles.textoPessoa}>{props.data.idade}</Text>
+     </View>   
+    )
+}
