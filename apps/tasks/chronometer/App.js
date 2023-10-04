202916b981e0a172067ceb7092a489b0bce807f4
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-10-04 06:31:46 -0300

    feat(tasks/chronometer) chronometer done

--- apps/tasks/chronometer/App.js
+++ apps/tasks/chronometer/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import MyChronometer from './src/MyChronometer';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor={'#7243E8'}
+      />
+      
+      <View style={{flex: 1}}>
+        <MyChronometer />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#5331A8',
+  }
 });
---
+++ apps/tasks/chronometer/src/MyChronometer.js
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+
+let date = new Date(0,0)
+let timerInterval = null
+
+export default function MyChronometer() {
+  const [timerText, setTimerText] = useState("00:00:00")
+  const [lastTime, setLastTime] = useState(null)
+  const [startOrPauseButtonLabel, setStartOrPauseButtonLabel] = useState("▶️")
+
+  function toggleStartOrPauseButtonLabel() {
+    if(startOrPauseButtonLabel === "⏸") {
+      pause()
+      setStartOrPauseButtonLabel("▶️")
+
+      return
+    }
+
+    start()
+    setStartOrPauseButtonLabel("⏸")    
+  }
+
+  function getFormattedDate() {
+    const seconds = String(date.getSeconds()).padStart(2, '00')
+    const minutes = String(date.getMinutes()).padStart(2, '00')
+    const hours = String(date.getHours()).padStart(2, '00')
+
+    return `${hours}:${minutes}:${seconds}`
+  }
+
+  function start() {
+    const ONE_SECOND = 1000
+
+    timerInterval = setInterval(() => {
+      date.setTime(date.getTime() + ONE_SECOND)
+
+      setTimerText(getFormattedDate)
+    }, ONE_SECOND)
+  }
+
+  function pause() {
+    clearInterval(timerInterval)
+    timerInterval = (null)
+  }
+
+  function reset() {
+    pause()
+    setTimerText("00:00:00")
+    date = new Date(0,0)
+    setStartOrPauseButtonLabel("▶️")
+    setLastTime(timerText)
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.timerWrapper}>
+        <Text style={styles.timerText}>{timerText}</Text>
+      </View>
+
+      <View style={styles.lastTimeWrapper}>
+        <Text style={styles.lastTimeText}>{lastTime ? `Último tempo: ${lastTime}` : ""}</Text>
+      </View>
+
+      <View style={styles.timerActions}>
+        <TouchableOpacity onPress={reset}>
+          <View>
+            <Text style={{fontSize: 50}}>🔁</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={toggleStartOrPauseButtonLabel}>
+          <View>
+            <Text style={{fontSize: 50}}>{startOrPauseButtonLabel}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  timerWrapper: {
+    flex: 6,
+    justifyContent: 'center'
+  },
+  timerText: {
+    fontSize: 80,
+    color: '#fff',
+    textAlign: 'center'
+  },
+  lastTimeWrapper: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  lastTimeText: {
+    fontSize: 18,
+    fontStyle: 'italic',
+    color: '#fff'
+  },
+  timerActions: {
+    flex: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  }
+})
